427d9dea360dc972fa53cd3ccf5a81f52834f37f
adriibc <92789192+adriibc@users.noreply.github.com>
2023-11-30 13:40:44 +0100

    Rellenado wander localiza

--- PL2/wander_localiza.m
+++ PL2/wander_localiza.m
@@ -1,17 +1,87 @@
 %Cargar el mapa
 %%%%%%%%%%%%%%%
+
+load OnlineSLAM_mapa.mat 
+show(map);
+
 %Crear el objeto VFH…y ajustar sus propiedades
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Inicializar el localizador AMCL (práctica 1)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+odometryModel = odometryMotionModel;
+odometryModel.Noise = [0.2 0.2 0.2 0.2];
+
+rangeFinderModel = likelihoodFieldSensorModel;
+rangeFinderModel.SensorLimits = [0 8];
+rangeFinderModel.Map = map;
+
+tftree = rostf;
+%Obtener transformada entre los frames del robot y del sensor_laser
+waitForTransform(tftree,'/robot0','/robot0_laser_1');
+sensorTransform = getTransform(tftree,'/robot0','/robot0_laser_1');
+
+% Get the euler rotation angles.
+laserQuat = [sensorTransform.Transform.Rotation.W sensorTransform.Transform.Rotation.X ...
+    sensorTransform.Transform.Rotation.Y sensorTransform.Transform.Rotation.Z];
+laserRotation = quat2eul(laserQuat, 'ZYX');
+
+% Setup the |SensorPose|, which includes the translation along base_link's
+% +X, +Y direction in meters and rotation angle along base_link's +Z axis
+% in radians.
+rangeFinderModel.SensorPose = ...
+    [sensorTransform.Transform.Translation.X sensorTransform.Transform.Translation.Y laserRotation(1)];
+
+amcl = monteCarloLocalization;
+amcl.UseLidarScan = true;
+
+amcl.MotionModel = odometryModel;
+amcl.SensorModel = rangeFinderModel;
+
+amcl.UpdateThresholds = [0.2,0.2,0.2];
+amcl.ResamplingInterval = 1;
+
+amcl.ParticleLimits = [500 50000];           % Minimum and maximum number of particles
+amcl.GlobalLocalization = true;      % global = true      local=false
+amcl.InitialPose = [0 0 0];              % Initial pose of vehicle   
+amcl.InitialCovariance = diag([1 1 1])*0.5; % Covariance of initial pose %eye(3)*0.5;
+
+visualizationHelper = ExampleHelperAMCLVisualization(map);
+
 %Rellenamos los campos por defecto de la velocidad del robot, para que la lineal
 %sea siempre 0.1 m/s
 %Bucle de control infinito
-while(1)
 
+while(1)
+    % Receive laser scan and odometry message.
+    scan = receive(sub_laser);
+    odompose = sub_odom.LatestMessage;
+    
+    %Crear objeto para almacenar el escaneo LiDAR 2-D
+    scans = lidarScan(scan);
+
+    % Compute robot's pose [x,y,yaw] from odometry message.
+    odomQuat = [odompose.Pose.Pose.Orientation.W, odompose.Pose.Pose.Orientation.X, ...
+        odompose.Pose.Pose.Orientation.Y, odompose.Pose.Pose.Orientation.Z];
+    odomRotation = quat2eul(odomQuat);
+    pose = [odompose.Pose.Pose.Position.X, odompose.Pose.Pose.Position.Y odomRotation(1)];
+    
+    [isUpdated,estimatedPose, estimatedCovariance] = amcl(pose, scans);
+    
+    % Drive robot to next pose.
+    %wander(wanderHelper);
+    
+    % Plot the robot's estimated pose, particles and laser scans on the map.
+    if isUpdated
+        i = i + 1
+        plotStep(visualizationHelper, amcl, estimatedPose, scans, i)
+    end
+    
  %Leer y dibujar los datos del láser en la figura ‘fig_laser’
 
  %Leer la odometría
+
+
  %Obtener la posición pose=[x,y,yaw] a partir de la odometría anterior
  %Ejecutar amcl para obtener la posición estimada estimatedPose y la
  %covarianza estimatedCovariance (mostrar la última por pantalla para
